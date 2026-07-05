5c96d147134e5f7e77d2d35bc3a14fc36e1e88d4
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-07-05 18:15:03 +0500

    Add calculator.py

---
+++ exercises/arithmatic_calculator.py
@@ -0,0 +1,55 @@
+# Helper function to create a border for console output visualization
+def separator():
+    print(58 * '=')
+
+def get_float_input(prompt):
+    while True:
+        num = input(prompt) 
+        try:
+            return float(num) 
+        except ValueError:
+            print("Invalid input! Please enter a valid number. ⚠")
+
+def get_operator(prompt):
+    while True: 
+        op = input(prompt)
+        if op in ['+', '-', '/', '*']: 
+            return op
+        print("Invalid operator! Please choose +, -, *, or /. ⚠")
+
+
+def addition(para1, para2):
+    return para1 + para2
+
+def subtraction(para1, para2):
+    return para1 - para2
+
+def multiplication(para1, para2):
+    return para1 * para2
+
+def division(para1, para2):
+    if para2 == 0:
+        return "Error (Cannot divide by zero)"
+    return para1 / para2
+
+separator()
+print("__________________________Calculator______________________\n")
+
+num1 = get_float_input('Enter first number: ')
+operator = get_operator('Enter the operation you want to perform (+, -, *, /): ')
+num2 = get_float_input('Enter second number: ')
+
+if operator == '+':
+    result = addition(num1, num2)
+    print(f"Addition of {num1} and {num2} is: {result}")
+elif operator == '-':
+    result = subtraction(num1, num2)
+    print(f"Subtraction of {num1} and {num2} is: {result}")
+elif operator == '*':
+    result = multiplication(num1, num2)
+    print(f"Product of {num1} and {num2} is: {result}")
+elif operator == '/':
+    result = division(num1, num2)
+    print(f"Division of {num1} and {num2} is: {result}")
+        
+separator()
