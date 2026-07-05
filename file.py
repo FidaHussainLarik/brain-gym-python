17ba6d693b6527e13e0fd17d8101fee641248043
FidaHussainLarik <fidahussainsthp23@gmail.com>
2026-07-05 12:28:54 +0500

    Count file

--- file.py
+++ file.py
@@ -5,11 +5,16 @@ print("______________________________________Working with file__________________
 # Reading file named 'ai_engineer_role.txt'
 
 my_file = open('ai_engineer_role.txt','r')
+count = 0
 
 for line in my_file:
+    count = count+1
     print(line)
 
 
+
+print("Counting lines in a file")
+print(f"There are {count} lines in my file 'ai_engineer_role.txt'")
 
 
 print("\n______________________________________Working with file______________________________________")
