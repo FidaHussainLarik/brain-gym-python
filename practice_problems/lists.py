6693a27178981f9a9509feef0d5fe8e3693dc9b6
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-07-06 10:48:20 +0500

    Parse strings from a list

---
+++ practice_problems/lists.py
@@ -0,0 +1,94 @@
+
+friends = ['Fida','Bob','Alice','Charle']
+
+# for friend in friends:
+#     print(friend)
+
+    
+# range function return a list of values start from 0 upto n-1
+# list1 = list(range(5))
+# print(list1)
+
+# COncatenation
+list1 = [1,2,3]
+list2 = [4,5,6]
+
+list3 = (list1+list2)
+
+# values greater then 2
+list4 = list3[2:]
+# print("List 4: ",list4)
+
+#Print all list methods
+# print(dir(list))
+
+# if 3 in list4:
+#     print("3 is present in the list")
+
+
+# IN opeartor
+# if 20 not in list4:
+#     print("20 is not in the list")
+
+# print(20 not in list4)
+
+
+# Computing average of user's entered values
+"""
+count = 0
+total = 0
+while True:
+    value = input("Enter a number: ").lower().strip()
+    if value == 'done':
+        break
+    value = float(value)
+    total = (total+value)
+    count = count+1
+
+
+average = (total/count)
+print(f"Total values entered by you: {count}")
+print(f"Average : {average}")
+"""
+
+# Creating an empty list()
+
+"""
+numslist = list()
+
+while True:
+    value = input("Enter a number: ").lower().strip()
+    if value == 'done':
+        break
+
+    try:
+        value = float(value)
+        numslist.append(value)
+    except:
+        print("Enter a valid number!")
+        continue
+average = sum(numslist) / len(numslist)
+print(f"Total values entered by you: {len(numslist)}")
+print(f"Average : {average}")
+"""
+
+
+
+# Strings and Lists
+# Converting strings into lists
+
+line = 'a lot of spaces'
+list1 = line.split()
+print(list1)
+
+line  = 'first,second,third,fourth'
+list1 = line.split(',')
+print(list1)
+
+# No delimeter/perimeter is assigned to split() function that's why it take ' ' space as a delimeter. But their is no any space in between values so string is converted into a single item list.
+
+line  = 'first,second,third,fourth'
+list1 = line.split()
+print(list1)
+
+
---
+++ practice_problems/parsing_files_into_lists.py
@@ -0,0 +1,38 @@
+
+
+
+
+try:
+    file_handle = open("C:/Future_Computer_Scientist/1_AI_Engineering/5_Repositories/brain-gym-python/data/mbox-short.txt")
+
+    # We add '../data/' to the filename to access it from the /exercises folder
+    # file_handle = open('../data/' + 'mbox-short.txt')
+except FileNotFoundError:
+    print(f"File 'mbox-short.txt' not found in the data folder.")
+    quit()
+
+# for line in file_handle:
+#     line = line.rstrip()
+#     if not line.startswith('From'):
+#         continue
+#     words = line.split()
+#     # date and time
+#     if len(words) > 2:
+#         print(words[2]) 
+
+
+# Extracting emails
+# Reset file pointer to the beginning
+file_handle.seek(0)
+
+
+for line in file_handle:
+    line = line.rstrip()
+    if not line.startswith('From'):
+        continue
+    words = line.split()
+    email = words[1]
+    peices_of_email = email.split('@')
+    print(peices_of_email[1])
+    
+
