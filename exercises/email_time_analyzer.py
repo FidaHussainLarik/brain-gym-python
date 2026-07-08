3a596bad0b0009bb8e68e8188ede6993c00d114a
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-07-08 23:39:37 +0500

    feat: add email_time_analyzer.py for email hour distribution analysis

---
+++ exercises/email_time_analyzer.py
@@ -0,0 +1,42 @@
+
+"""
+
+10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+
+"""
+
+file_name = input("Enter file:")
+if len(file_name) < 1:
+    file_name = "mbox-short.txt"
+
+
+
+try:
+
+    file_handle = open(file_name)
+
+    # 'hour' stores hours only
+    # 'time' stores hours, minute and seconds
+    hour = str()
+    time = list()
+    hours_count = dict()
+    
+    for line in file_handle:
+        if not line.startswith('From '):
+            continue
+
+        words = line.split()
+        time = words[5].split(':')
+        hour = time[0]
+
+        if len(words) >= 1:
+            hours_count[hour] = hours_count.get(hour, 0) + 1
+
+    for (key,value) in sorted(hours_count.items()):
+        print(key,value)
+
+except FileNotFoundError:
+    print("File not found!")
+   
