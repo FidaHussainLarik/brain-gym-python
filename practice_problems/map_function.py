6ea1ab14b21ff033f75d92c669d5116038ed6ec4
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-07-12 13:09:55 +0500

    add map_function.py

---
+++ practice_problems/map_function.py
@@ -0,0 +1,25 @@
+nums = ["1", "2", "3"]
+
+print(nums)
+
+
+"""
+        working of map()
+        map(int, nums)
+
+"10"  ---> int() ---> 10
+
+"20"  ---> int() ---> 20
+
+"30"  ---> int() ---> 30
+
+
+    returns a list of numbers
+
+
+"""
+
+result = map(int, nums)
+
+
+print(list(result))
