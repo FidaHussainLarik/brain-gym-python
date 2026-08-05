556e6e375f7c79473ec11f716cc03a1aedf856b9
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-08-05 21:03:17 +0500

    no change

--- practice_problems/program_that_surf_the_web.py
+++ practice_problems/program_that_surf_the_web.py
@@ -4,6 +4,21 @@ file_handle = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
 
 print()
+counts = dict()
+
 for line in file_handle:
-    print(line.decode().strip())
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+
+sorted_tup_list = list()
+
+for (key,value) in counts.items():
+    sorted_tup_list.append((value,key))
+
+sorted_tup_list.sort(reverse=True)
+for key,value in sorted_tup_list:
+    print(key,value)
+
 print()
