1ceba648885151185fb1e526a82668f8d5609109
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-08-07 01:28:28 +0500

    feat: count words using dictionary

--- networked_programs/urllib_lib.py
+++ networked_programs/urllib_lib.py
@@ -4,8 +4,25 @@ file_handle = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
 print()
 
+counts = dict()
+
 for line in file_handle:
     line = line.decode().strip()
+    words = line.split()
+
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
     print(line)
 
+
+print("DICTIONARY OF WORDS")
+tup_list = list()
+for key, val in counts.items():
+    tup_list.append((val,key))
+
+tup_list.sort(reverse=True)
+
+for val, key in tup_list:
+    print(key,val)
 print()
