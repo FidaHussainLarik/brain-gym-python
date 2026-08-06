bdcbe28c79ae5a4b2f81fdd054bc139c66561e5a
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-08-07 00:55:10 +0500

    refector: add explanatory comment

--- networked_programs/exercise1.py
+++ networked_programs/exercise1.py
@@ -5,8 +5,9 @@ mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
+print()
 while True:
-    data = mysock.recv(512)
+    data = mysock.recv(512) # receives data in 512-character chunks
     if len(data) < 1:
         break
     print(data.decode(), end='')
